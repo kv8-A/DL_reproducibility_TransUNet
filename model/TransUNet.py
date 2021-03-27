7e00e420169179afed3212fa9b91956c19f99cd9
casperteirlinck <casperteirlinck@gmail.com>
2021-03-27 18:51:42 +0100

    updated to comply more with paper

--- model/TransUNet.py
+++ model/TransUNet.py
@@ -1,55 +1,40 @@
 """ 
 For reconstructung the UNet part, we look at the decoder structure defined in the paper, and existing pytorch UNet implementations and paper.
-Ref UNet paper: https://arxiv.org/abs/1505.04597
-Ref UNet code: https://github.com/milesial/Pytorch-UNet
+Ref UNet paper: https://arxiv.org/abs/1505.04597 # [paper]
+Ref UNet code: https://github.com/milesial/Pytorch-UNet # [self search for pytorch impl.]
 
 We can see from the paper that the UNet architecture has:
     - 4 upsampling blocks
     - always kernel size 3, so padding is always 1, stride is assumed 1(not mentioned in paper)
     - 512 channels at the bottom
     - the following decoder channel sizes are: 256 -> 128 -> 64 -> 16
-    - the channels for the skip connections are: 512 -> 256 -> 64 -> 0 NOTE: Why?
     - 3 skip connections
 
-so the decoder blocks are:
-    - inoutres  | ksize | inoutch  | skipch
-    - 1/16->1/8 | 3x3   | 512->256 | 512
-    - 1/8 ->1/4 | 3x3   | 256->128 | 256
-    - 1/4 ->1/2 | 3x3   | 128->64  | 64 NOTE: Why not 128?
-    - 1/2 ->1/1 | 3x3   | 64->16   | 0
-
-according to UNet, a decoder block consists of: upsample->conv->bn->relu->conv->bn->relu
+according to UNet ref code, a decoder block consists of: upsample->conv->bn->relu->conv->bn->relu
+however the paper suggests a decoder block of: upsample->conv_relu
 according to the paper, the upsampling is done bilinearly
 
-so a single decoder block consists of: (ref. UNet)
-    - upsample : bilinear | resolution x2
-    - (add skip to input if skip is not None (using torch.cat([x, skip, dim=1])))
-    - conv     : (inch+skipch)->outch | 3x3
-    - bn       : outch
-    - ReLU
-    - conv     : outch->outch | 3x3
-    - bn       : outch
-    - ReLU
-
-the segmentation head definition is taken from UNet as well:
-    - conv : 16->2 | 1x1 NOTE: standard UNet uses 1x1 kernel for segmentation head while code fot TransUNet uses 3x3 (not mentioned in paper) !!
+the segmentation head definition is taken from UNet ref code as well
+as it is not discussed in the paper:
+    - conv : 16->2 | 1x1
 
 """
 
 import torch
+import torchvision
 import torch.nn as nn
 
 """
-UNet Decoder block
+Decoder block
 
-- upsample : bilinear | resolution x2
-- (add skip to input if skip is not None (using torch.cat([x, skip, dim=1])))
-- conv     : (inch+skipch)->outch | 3x3
-- bn       : outch
-- ReLU
-- conv     : outch->outch | 3x3
-- bn       : outch
-- ReLU
+According to paper, made clear in text and by Fig1, they use for 1 of the 4 blocks:
+1. upsample : bilinear | resolution x2
+2. (add skip connection) # [UNet, paper Fig1]
+3. conv     : (inch+skipch)->outch | 3x3
+4. ReLU
+
+NOTE: deviation from UNet paper: UNet adds batchnorm before relu [UNet code]
+      and has an intermediate step resulting in another conv->bn->relu added to the sequence
 """
 class DecoderBlock(nn.Module):
 
@@ -58,27 +43,17 @@ class DecoderBlock(nn.Module):
         
         # 2x Bilinear upsampling
         self.upsample = nn.Upsample(
-            scale_factor=2,
-            mode='bilinear'
+            scale_factor=2, # [paper]
+            mode='bilinear' # [paper]
         )
-        # 3x3 Convolutional, also accepting skip connection
-        self.conv1 = nn.Conv2d(
-            in_channels=(in_channels+skip_channels), 
+        # 3x3 Convolutional, accepting the skip connection
+        self.conv = nn.Conv2d(
+            in_channels=(in_channels+skip_channels),
             out_channels=out_channels,
-            kernel_size=3,
+            kernel_size=3, # [paper]
             padding=1,
             stride=1
         )
-        # 3x3 Convolutional
-        self.conv2 = nn.Conv2d(
-            in_channels=out_channels, 
-            out_channels=out_channels,
-            kernel_size=3,
-            padding=1,
-            stride=1
-        )
-        # BatchNorm
-        self.bn = nn.BatchNorm2d(out_channels)
         # ReLU activation
         self.relu = nn.ReLU()
 
@@ -86,26 +61,17 @@ class DecoderBlock(nn.Module):
         # Upsample
         x = self.upsample(x)
         # Add skip connection
-        x = torch.cat([x, skip], dim=1)
-        # Conv 1
-        x = self.conv1(x)
-        # BatchNorm 1
-        x = self.bn(x)
-        # ReLU 1
-        x = self.relu(x)
-        # Conv 2
-        x = self.conv2(x)
-        # BatchNorm 2
-        x = self.bn(x)
-        # ReLU 2
+        if skip != None: x = torch.cat([x, skip], dim=1)
+        # Conv
+        x = self.conv(x)
+        # ReLU
         x = self.relu(x)
 
 """ 
 The segmentation head
-converts back to final output image with segmented labels
+converts input back to final output image with segmented labels
 
-definition is taken from UNet
-- conv : 16->2 | 1x1 NOTE: standard UNet uses 1x1 kernel for segmentation head while code fot TransUNet uses 3x3 (not mentioned in paper) !!
+- conv : 16->2 | 1x1 # [UNet code]
 """
 class SegmentationHead(nn.Module):
 
@@ -134,6 +100,9 @@ from (n_patch, D) -> (D, H/16, W/16) -> (512, H/16, W/16)
 class ReshapeBlock(nn.Module):
     ...
 
+"""
+
+"""
 class TransUNet(nn.Module):
 
     def __init__(self):
@@ -143,12 +112,12 @@ class TransUNet(nn.Module):
         self.reshapeBlock = ReshapeBlock()
 
         """
-        Decoder blocks according to paper:
-        - inoutres  | ksize | inoutch 
-        - 1/16->1/8 | 3x3   | 512->256
-        - 1/8 ->1/4 | 3x3   | 256->128
-        - 1/4 ->1/2 | 3x3   | 128->64 
-        - 1/2 ->1/1 | 3x3   | 64->16
+        Decoder block channels according to paper:
+        - inoutch 
+        - 512->256
+        - 256->128
+        - 128->64 
+        - 64->16
         NOTE: what are the skip connection channels? Get it from Resnet output?
         """
         # Decoder blocks
@@ -176,7 +145,7 @@ class TransUNet(nn.Module):
         # Segmentation head
         self.segmentationHead = SegmentationHead(
             in_channels=16,
-            out_channels=... # 2 channels?
+            out_channels=3 # assumtopn that output is rgb
         )
     
     def forward(self, x):
@@ -190,7 +159,7 @@ class TransUNet(nn.Module):
         x = decoderBlock1(x, skip=...)
         x = decoderBlock2(x, skip=...)
         x = decoderBlock3(x, skip=...)
-        x = decoderBlock4(x, skip=...)
+        x = decoderBlock4(x, skip=None)
         
         # Segmentation head
         x = self.segmentationHead(x)
